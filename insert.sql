214123083d1255197e757c89a8cb6873a2dab1da
Filip Stamenković <brunobusic20@gmail.com>
2023-12-14 20:58:51 +0100

    ucenje inserta

---
+++ insert.sql
@@ -0,0 +1,14 @@
+﻿use edunovawp3;
+
+select * from smjerovi;
+
+insert into smjerovi(naziv,trajanje,cijena,vaucer)
+values('web programiranje',225,1859.45,1);
+
+--losa sintaksa
+insert into smjerovi values
+('web dizajn',null,null,null);
+
+
+--minimalna dobra sintaksa
+insert into smjerovi(naziv) values ('ćžžćžć');
