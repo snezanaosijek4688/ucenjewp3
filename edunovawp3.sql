66ee3d978ff15f43ad4647d2822001cacea94779
Filip Stamenković <brunobusic20@gmail.com>
2023-12-14 20:03:09 +0100

    kreiranje tablica i veza izmedju tablica

---
+++ edunovawp3.sql
@@ -0,0 +1,24 @@
+--ovo je komentar
+--create database edunovawp3;
+--drop database edunovawp3
+--use edunovawp3;
+create table smjerovi(
+sifra int,
+naziv varchar(50),
+trajanje int,
+cijena decimal (18,2),
+vaucer bit 
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
