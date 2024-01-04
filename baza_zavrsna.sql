15165e5d2ba9f5b353452f59fea063a09e592329
sneki <152445228+snezanaosijek4688@users.noreply.github.com>
2024-01-04 14:30:15 +0100

    zavrsna baza

---
+++ baza_zavrsna.sql
@@ -0,0 +1,39 @@
+use master;
+go
+drop database if exists
+teretana;
+go
+--ovo je komentar
+create database teretana collate Croatian_CI_AS;
+go
+--drop database teretana;
+--use master
+use teretana;
+
+
+
+create table korisnici(
+sifra int not null primary key identity(1,1),
+ime varchar(50),
+prezime varchar(50),
+datumrodenja datetime,
+email varchar(50),
+telefon varchar(50),
+trener_sifra int not null
+
+);
+
+
+create table treneri(
+sifra int not null primary key identity (1,1),
+ime varchar(50),
+prezime varchar(50),
+specijalizacija varchar(50),
+radnovrijeme datetime,
+
+
+);
+
+
+
+alter table korisnici add foreign key (trener_sifra) references treneri(sifra);
